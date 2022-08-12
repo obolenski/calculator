b8894a3e05c6f3a1ac17d3413bfa9facdfee9776
obolenski <kornet.obolenski@gmail.com>
2022-08-12 22:25:15 +0400

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,46 @@
-import type { Component } from 'solid-js'
+import { Component, Suspense } from 'solid-js'
+import { useRoutes } from '@solidjs/router'
+
+import Header from './components/Header'
 
 import { ThemeProvider, createTheme } from '@suid/material/styles'
+import CssBaseline from '@suid/material/CssBaseline'
+import Box from '@suid/material/Box'
+import useMediaQuery from '@suid/material/useMediaQuery'
+import LinearProgress from '@suid/material/LinearProgress'
 
-import OhmsLaw from './pages/OhmsLaw/OhmsLaw'
-import Header from './components/Header'
-import styles from './App.module.css'
+import routes from './routes/routes'
+
+const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
 
 const darkTheme = createTheme({
   palette: {
+    // mode: prefersDarkMode() ? 'dark' : 'light',
     mode: 'dark',
   },
 })
 
 const App: Component = () => {
+  const Routes = useRoutes(routes)
   return (
     <ThemeProvider theme={darkTheme}>
-      <div class={styles.App}>
+      <CssBaseline />
+      <Box>
         <Header />
-        <OhmsLaw />
-      </div>
+        <Box
+          component="main"
+          sx={{
+            flexGrow: 1,
+            p: 3,
+            alignContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Suspense fallback={<LinearProgress />}>
+            <Routes />
+          </Suspense>
+        </Box>
+      </Box>
     </ThemeProvider>
   )
 }
---
+++ client/src/routes/routes.tsx
@@ -0,0 +1,23 @@
+import { lazy } from 'solid-js'
+
+const routes = [
+  {
+    path: '/',
+    component: lazy(() => import('../pages/Home')),
+    linkText: 'Home',
+    excludeFromNav: true,
+  },
+  {
+    path: '/ohm',
+    component: lazy(() => import('../pages/OhmsLaw/OhmsLaw')),
+    linkText: "Ohm's Law",
+  },
+  {
+    path: '**',
+    component: lazy(() => import('../pages/NotFound')),
+    linkText: '',
+    excludeFromNav: true,
+  },
+]
+
+export default routes
