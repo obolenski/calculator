c4b0b5de68301df1c0f53dc6a66e2a5f127983f8
obolenski <kornet.obolenski@gmail.com>
2022-08-16 21:09:49 +0400

    add home button to 404 page

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'solid-js'
 import Page from '../components/Page'
 import verySadEmoji from '../assets/verySadEmoji.png'
+import Button from '@suid/material/Button'
 
 const NotFound: Component = () => {
   const currentPath = window.location.href
@@ -9,7 +10,9 @@ const NotFound: Component = () => {
     <p>
       Page <i>{currentPath}</i> not found
     </p>,
-    <p>Check the URL</p>,
+    <Button href="/" aria-label="home">
+      Back to homepage
+    </Button>,
   ]
   const leftElements = [<img src={verySadEmoji} width="100" />]
   const rightElements = [<img src={verySadEmoji} width="100" />]
